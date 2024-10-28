b95b08509b2c53c2f75b60b345dd61356e8e7ac9
Steven Shen <zhexiushen@gmail.com>
2024-10-28 18:22:49 +0800

    Added validator to custom directory name

---
+++ src/dir_name_combobox.cc
@@ -0,0 +1,105 @@
+#include <dir_name_combobox.h>
+#include <spdlog/spdlog.h>
+
+#include <QLineEdit>
+#include <QSettings>
+
+namespace
+{
+constexpr auto DIR_NAME = "dir_name";
+constexpr auto DIR_DATE = "dir_date";
+constexpr auto MAX_DIR_NAME_LEN = 255;
+}  // namespace
+
+bool DirNameComboBox::valid_dir_name_from_user_string(const QString &text)
+{
+    if (text.endsWith('.') || text.endsWith(' ')) {
+        return false;
+    }
+
+    static const auto reserved_names = {tr("CON"),  tr("PRN"),  tr("AUX"),  tr("NUL"),  tr("COM"),
+                                        tr("COM0"), tr("COM1"), tr("COM2"), tr("COM3"), tr("COM4"),
+                                        tr("COM5"), tr("COM6"), tr("COM7"), tr("COM8"), tr("COM9"),
+                                        tr("LPT0"), tr("LPT1"), tr("LPT2"), tr("LPT3"), tr("LPT4"),
+                                        tr("LPT5"), tr("LPT6"), tr("LPT7"), tr("LPT8"), tr("LPT9")};
+    for (const auto &reserved : reserved_names) {
+        if (text.startsWith(reserved, Qt::CaseInsensitive)) {
+            if (text.size() == reserved.size() || text[reserved.size()] == '.') {
+                return false;
+            }
+        }
+    }
+
+    static const auto validator =
+        QRegularExpression("^[a-zA-Z0-9_-]+$", QRegularExpression::CaseInsensitiveOption);
+
+    return validator.match(text).hasMatch();
+}
+
+void DirNameComboBox::handle_editing_finished()
+{
+    auto default_dir_name = tr("directory_name");
+    auto text = currentText();
+    if (!valid_dir_name_from_user_string(text)) {
+        QSettings("KonteX", "VC").setValue(DIR_NAME, default_dir_name);
+        setItemText(1, default_dir_name);
+        setCurrentText(default_dir_name);
+        spdlog::info("Saved custom directory name: {}", default_dir_name.toStdString());
+        return;
+    }
+
+    auto dir_name = text.trimmed();
+    setItemText(1, dir_name);
+    QSettings("KonteX", "VC").setValue(DIR_NAME, dir_name);
+    spdlog::info("Saved custom directory name: {}", dir_name.toStdString());
+}
+
+DirNameComboBox::DirNameComboBox(QWidget *parent) : QComboBox(parent)
+{
+    QSettings settings("KonteX", "VC");
+    auto dir_date = settings.value(DIR_DATE, false).toBool();
+    auto dir_name = settings.value(DIR_NAME, tr("directory_name")).toString();
+    settings.setValue(DIR_DATE, dir_date);
+    settings.setValue(DIR_NAME, dir_name);
+
+    addItem(tr("YYYY-MM-DD_HH-MM-SS"));
+    addItem(dir_name);
+
+    setInsertPolicy(QComboBox::NoInsert);
+    setEditable(!dir_date);
+    setCurrentIndex(dir_date ? 0 : 1);
+
+    // TODO: duplicate code inside currentIndexChanged signal slot.
+    // index 0 has no lineEdit(), only connect slot when index is 1, which is custom dir name
+    // code works fine with duplicate &QLineEdit::editingFinished, but it is irritating.
+    if (!dir_date) {
+        lineEdit()->setMaxLength(MAX_DIR_NAME_LEN);
+        connect(
+            lineEdit(), &QLineEdit::editingFinished, this, &DirNameComboBox::handle_editing_finished
+        );
+    }
+
+    connect(this, &QComboBox::currentIndexChanged, [this](int index) {
+        auto dir_date = (index == 0);
+        QSettings("KonteX", "VC").setValue(DIR_DATE, dir_date);
+        setEditable(!dir_date);
+        spdlog::info("Saved dir date : {}", dir_date);
+
+        if (dir_date) {
+            setStyleSheet("");
+            return;
+        }
+
+        lineEdit()->setMaxLength(MAX_DIR_NAME_LEN);
+        // disconnect(lineEdit(), &QLineEdit::editingFinished, nullptr, nullptr);
+        connect(
+            lineEdit(), &QLineEdit::editingFinished, this, &DirNameComboBox::handle_editing_finished
+        );
+    });
+    connect(this, &QComboBox::editTextChanged, [this](const QString &text) {
+        if (currentIndex() == 0) return;
+        valid_dir_name_from_user_string(text)
+            ? setStyleSheet("")
+            : setStyleSheet("QComboBox { background-color: #FFE4E1; }");
+    });
+}
---
+++ src/dir_name_combobox.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <QComboBox>
+
+
+class DirNameComboBox : public QComboBox
+{
+    Q_OBJECT
+public:
+    DirNameComboBox(QWidget *parent = nullptr);
+    ~DirNameComboBox() = default;
+
+private:
+    bool valid_dir_name_from_user_string(const QString &text);
+private slots:
+    void handle_editing_finished();
+};
