4199c486dfa8b780ca230b424264f13fe0851a07
Steven Shen <zhexiushen@gmail.com>
2025-06-02 13:03:18 +0800

    Version 0.1.1
    Merge: retry server to prevent sudden fail
    Delete camera object to release port
    
    Former-commit-id: 1a94948c9e5721ec9e462996b480e37c65a6b8b9

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,6 @@ from os.path import join
 
 class ThorVision(ConanFile):
     name = "ThorVision"
-    version = "0.1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "VirtualRunEnv"
     license = "LGPL-3.0-or-later"
--- thorvision/src/app.cc
+++ thorvision/src/app.cc
@@ -56,7 +56,7 @@ App::App(int &argc, char **argv) : QApplication(argc, argv)
         fs::remove(log_path);
     }
 
-    setApplicationVersion("0.1.0");
+    setApplicationVersion("0.1.1");
     setApplicationName(QString("Thor Vision-%1-beta").arg(applicationVersion()));
 
     auto logger = logs::setup_logger(log_path.generic_string());
