0162df49eefb675e595a38b09bfdcd50d31cb098
Steven Shen <zhexiushen@gmail.com>
2025-12-30 12:51:03 +0800

    Upgrade libxvc to 0.2.0 Add loading default config function fix: import status drawer disappears when the window height is reduced fix: xdaq connected/connecting text hint icons are clearer now
    Former-commit-id: 048f9722aac3ba821df8639079053271d72ee849

--- conanfile.py
+++ conanfile.py
@@ -22,8 +22,8 @@ class ThorVision(ConanFile):
         self.requires("spdlog/1.13.0")
         self.requires("nlohmann_json/3.11.3")
         # self.requires("json-schema-validator/2.3.0")
-        self.requires("libxvc/0.1.2")
-        self.requires("xdaqmetadata/0.1.2")
+        self.requires("libxvc/0.2.0")
+        self.requires("xdaqmetadata/0.1.1")
         self.requires("oatpp/1.3.0.latest")
 
     def layout(self):
